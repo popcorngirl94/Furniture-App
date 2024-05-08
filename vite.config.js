c570b8a3383c142e1fca6e83ef2681382737463f
Grishma Shrestha <64399464+popcorngirl94@users.noreply.github.com>
2024-05-08 08:36:10 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
      build: {
        outDir: 'public' 
-       }
+       },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
